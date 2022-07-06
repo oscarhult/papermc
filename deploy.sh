9c4c13112635835a25db4744466e7eede0f4d9be
Oscar Hult <oscar@hult.it>
2022-07-06 13:10:01 +0200

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+version=1.19
+build=$(curl -fsL https://api.papermc.io/v2/projects/paper/versions/${version}/builds | jq '.builds | max_by(.build) | .build')
+id=$(curl -fsL https://hub.docker.com/v2/repositories/oscarhult/papermc/tags/${version}-${build} | jq '.id')
+
+if [[ ! ${id} ]]; then
+  tag1=oscarhult/papermc:${version}-${build}
+  tag2=oscarhult/papermc:${version}
+  tag3=oscarhult/papermc:latest
+
+  docker build \
+    --tag ${tag1} \
+    --tag ${tag2} \
+    --tag ${tag3} \
+    --build-arg PAPERMC_VERSION=${version} \
+    --build-arg PAPERMC_BUILD=${build} \
+    'https://github.com/oscarhult/papermc.git'
+
+  docker login -u oscarhult --password-stdin <<< ${DOCKER_ACCESS_TOKEN}
+
+  docker push ${tag1}
+  docker push ${tag2}
+  docker push ${tag3}
+else
+    curl -fsL https://api.papermc.io/v2/projects/paper/versions/${version}/builds | jq '.builds | max_by(.build)'
+    curl -fsL https://hub.docker.com/v2/repositories/oscarhult/papermc/tags/${version}-${build} | jq '.'
+fi
